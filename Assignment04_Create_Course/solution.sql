dcf1256977ea60285ffb98fe50e6d680b2a67ad9
1U25CA039-JEEVITHA <jeevithaj3037@gmail.com>
2026-09-09 13:37:03 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,7 @@
-DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-
--- Create Course table
-
--- Insert three records
-
--- Display structure
+CREATE TABLE Course (CourseID INT(5) PRIMARY KEY,CourseName VARCHAR(20), Credits INT, DepartmentID INT(5));
+INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
+VALUES(201, 'DBMS', 4, 101),(202, 'Java', 3, 102),(203, 'Python', 4, 101);
+SELECT * FROM Course;
+DESCRIBE Course;
